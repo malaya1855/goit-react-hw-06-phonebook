63022ab5ec81bb940e727885f9b68b35fb99e411
malaya1855 <121491813+malaya1855@users.noreply.github.com>
2023-11-23 15:53:13 +0200

    phone mask

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -2,15 +2,15 @@ import { Form, Input, ButtonForm } from 'components';
 import { nanoid } from 'nanoid';
 import { useDispatch, useSelector } from 'react-redux';
 import { addContact } from 'redux/slice/contactsSlice';
+import InputMask from 'react-input-mask';
 
 export const ContactForm = () => {
   const allContacts = useSelector(state => state.contacts);
 
   const dispatch = useDispatch();
-
   const onSubmitForm = ev => {
     ev.preventDefault();
-    const name = ev.currentTarget.elements.name.value;
+    const name = ev.currentTarget.elements.name.value.trim();
     const number = ev.currentTarget.elements.number.value;
 
     const newContact = { id: nanoid(5), name, number };
@@ -32,20 +32,24 @@ export const ContactForm = () => {
           type="text"
           name="name"
           placeholder="Name"
-          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          // pattern="^[a-zA-Zа-яА-Я]+(([' \-][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
           title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
           required
         />
       </label>
       <label htmlFor="number">
-        <Input
-          type="tel"
-          name="number"
-          placeholder="Phone number"
-          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-          title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-          required
-        />
+        <InputMask mask="+399 (99) 9999999" maskPlaceholder="-">
+          {() => (
+            <Input
+              type="tel"
+              name="number"
+              placeholder="Phone number"
+              // pattern="\+?\d{1,4}?[ .\-\s]?\(?\d{1,3}?\)?[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,9}"
+              title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+              required
+            />
+          )}
+        </InputMask>
       </label>
       <ButtonForm type="submit">Add contact</ButtonForm>
     </Form>
